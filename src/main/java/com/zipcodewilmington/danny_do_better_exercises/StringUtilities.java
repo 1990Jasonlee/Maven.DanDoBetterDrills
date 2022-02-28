222b3f9d35985889004abad289d7d185e7493a32
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-02-28 15:19:41 -0500

    complete

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -65,9 +65,12 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        if (inputValue.length() % 2 == 0) {
+            return inputValue.charAt(inputValue.length() / 2 - 1);
+        } else {
+            return inputValue.charAt(inputValue.length() / 2);
+        }
     }
-
     /**
      * @param spaceDelimitedString a string, representative of a sentence, containing spaces
      * @return the first sequence of characters
